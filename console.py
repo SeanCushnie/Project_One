214d95f55427d5aafdb3e10389e0dd440bfc7169
SeanCushnie <133601523+SeanCushnie@users.noreply.github.com>
2023-06-09 21:02:18 +0100

    added function to display out of stock... although this is appearing on the same line.

--- console.py
+++ console.py
@@ -15,6 +15,7 @@ manufacturer_repository.select_all()
 
 product_1 = Product(manufacturer_1, "501/453/UNIV/A/M20/BRASS", "20mm gland, suitable for use in hazardous areas where cold-flow could be a potential issue", 10, 5, 15)
 product_repository.save(product_1)
-product_2 = Product(manufacturer_2, "EVXB/20L/LE", "Non-Emergency Floodlight, Zone 1, 20,980lm", 5, 2500, 3500)
+product_2 = Product(manufacturer_2, "EVXB/20/LE", "Non-Emergency Floodlight, Zone 1, 20,980lm", 5, 2500, 3500)
 product_repository.save(product_2)
-
+product_3 = Product(manufacturer_2, "EVXB/10/LE", "Non-Emergency Floodlight, Zone 1, 20,980lm", 0, 2500, 3500)
+product_repository.save(product_3)
