47d458bf81baeead9b419eb75d4f4263dd510f85
SeanCushnie <133601523+SeanCushnie@users.noreply.github.com>
2023-06-09 16:12:47 +0100

    added another base product

--- console.py
+++ console.py
@@ -13,7 +13,8 @@ manufacturer_2 = Manufacturer("Chalmit")
 manufacturer_repository.save(manufacturer_2)
 manufacturer_repository.select_all()
 
-product_1 = Product(manufacturer_1, "501/453/UNIV/A/M20/BRASS", "20mm gland, suitable for use in hazardous areas where cold-flow could be a potential issue", 10, 2, 15)
+product_1 = Product(manufacturer_1, "501/453/UNIV/A/M20/BRASS", "20mm gland, suitable for use in hazardous areas where cold-flow could be a potential issue", 10, 5, 15)
 product_repository.save(product_1)
-
+product_2 = Product(manufacturer_2, "Evolution X", "Floodlight", 5, 2500, 3500)
+product_repository.save(product_2)
 
