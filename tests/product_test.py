302673184cadaeb0d0c27006af6ee194ec04be9d
SeanCushnie <133601523+SeanCushnie@users.noreply.github.com>
2023-06-14 11:00:58 +0100

    added some product tests

---
+++ tests/product_test.py
@@ -0,0 +1,23 @@
+import unittest 
+from models.product import Product
+from models.manufacturer import Manufacturer
+
+class TestProduct(unittest.TestCase):
+    def setUp(self):
+        self.product = Product(1, "EVXB/20L/LE", "floodlight",5,10,20)
+        self.manufacturer = Manufacturer("Chalmit", 1)
+
+    def test_product_has_manufacturer(self):
+        self.assertEqual(1, self.product.manufacturer)
+    
+    def test_product_has_title(self):
+        self.assertEqual("EVXB/20L/LE", self.product.title)
+    
+    def test_product_has_description(self):
+        self.assertEqual("floodlight", self.product.description)
+    
+    def test_product_has_stock_quantity(self):
+        self.assertEqual(5, self.product.stock_quantity)
+    
+    
+    
